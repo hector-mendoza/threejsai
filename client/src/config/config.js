02fd2b9c32a984ac3bd3aad086b25291e212f329
hector-mendoza <hec1702@gmail.com>
2023-04-20 17:35:09 -0600

    Adding "render"

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://threejs-tr3q.onrender.com",
+    backendUrl: "https://threejs-dalle.onrender.com",
   },
 };
 
